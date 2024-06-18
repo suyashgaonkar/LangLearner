bbb0257c9491a0fc84bd2b33d96d9eba7827a0db
Denyyyys <denys.fokashchuk@gmail.com>
2024-06-18 02:04:34 +0100

    Add repository for language model

---
+++ backend/Database/Repositories/LanguageRepository.cs
@@ -0,0 +1,31 @@
+﻿using LangLearner.Models.Entities;
+using System.Linq;
+
+namespace LangLearner.Database.Repositories
+{
+    public interface ILanguageRepository
+    {
+        IEnumerable<Language> GetAll();
+
+        Language? GetLanguageByAny(string value);
+    }
+    public class LanguageRepository : ILanguageRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LanguageRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Language> GetAll()
+        {
+            return _context.Languages.AsEnumerable();
+        }
+
+        public Language? GetLanguageByAny(string value)
+        {
+            return _context.Languages
+                .FirstOrDefault(l => l.Code == value || l.Name == value || l.NativeName == value);
+        }
+    }
+}
