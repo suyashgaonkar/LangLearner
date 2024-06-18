17cfecff241c317a0a7b241490551cb06bdd1ae9
Denyyyys <denys.fokashchuk@gmail.com>
2024-06-18 02:07:31 +0100

    Configured build file

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,15 +1,29 @@
+using LangLearner.Database;
+using LangLearner.Database.Seeders;
+using Microsoft.EntityFrameworkCore;
+using NLog.Extensions.Logging;
+using System;
+using NLog.Web;
+using LangLearner.Services;
+using LangLearner.Database.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Host.UseNLog();
+builder.Services.AddScoped<ILanguagesService, LanguagesService>();
+builder.Services.AddScoped<ILanguageRepository, LanguageRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +35,21 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Seed data
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<AppDbContext>();
+var logger = services.GetService<ILogger>();
+
+try
+{
+    await context.Database.MigrateAsync();
+    await DatabaseInitializer.SeedDataAsync(context);
+}
+catch (Exception ex)
+{
+    app.Logger.LogError(ex, " and error occured during migration or seeding");
+}
 
 app.Run();
