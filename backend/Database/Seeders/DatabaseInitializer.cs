dee35dc9a45ccb45f7913435cd782b309538c351
Denyyyys <denys.fokashchuk@gmail.com>
2024-06-18 02:05:05 +0100

    Add basic seeder for languages

---
+++ backend/Database/Seeders/DatabaseInitializer.cs
@@ -0,0 +1,33 @@
+﻿using LangLearner.Models.Entities;
+
+namespace LangLearner.Database.Seeders
+{
+    public static class DatabaseInitializer
+    {
+
+        public static async Task SeedDataAsync(AppDbContext context)
+        {
+            await SeedDefaultLanguagesAsync(context);
+        }
+
+        private static async Task SeedDefaultLanguagesAsync(AppDbContext context)
+        {
+            if(!context.Languages.Any())
+            {
+                var languages = new List<Language>
+                {
+                    new Language {Code="en", Name="English", NativeName="English"},
+                    new Language {Code="es", Name="Spanish", NativeName="Espańol"},
+                    new Language {Code="pl", Name="Polish", NativeName="Polski"},
+                    new Language {Code="de", Name="German", NativeName="Deutsch"},
+                    new Language {Code="ua", Name="Ukrainian", NativeName="Українська"},
+                };
+
+                context.Languages.AddRange(languages);
+                if(context.ChangeTracker.HasChanges()) await context.SaveChangesAsync();
+            }
+        }
+
+
+    }
+}
