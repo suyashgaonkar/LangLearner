39f6d6d115d0a6d7c42606bb5d3b1c1e3d9570c6
Denyyyys <denys.fokashchuk@gmail.com>
2024-07-05 21:20:09 +0100

    Add middleware for handling exceptions

---
+++ backend/Exceptions/GeneralAPIException.cs
@@ -0,0 +1,8 @@
+﻿namespace LangLearner.Exceptions
+{
+    public class GeneralAPIException : Exception
+    {
+        public int StatusCode { get; set; } = 500;
+        public GeneralAPIException(string message) : base(message) { }
+    }
+}
---
+++ backend/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using LangLearner.Exceptions;
+using LangLearner.Models.Dtos.Responses;
+using System.Text.Json;
+
+namespace LangLearner.Middlewares
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public ErrorHandlingMiddleware()
+        {
+            
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (GeneralAPIException e)
+            {
+                context.Response.StatusCode = e.StatusCode;
+                context.Response.ContentType = "application/json";
+                var errorResponse = new ApiError { ErrorMessage = e.Message, StatusCode = e.StatusCode };
+                var errorJson = JsonSerializer.Serialize(errorResponse);
+                await context.Response.WriteAsync(errorJson);
+            }
+            catch (Exception e)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync(e.Message ?? "Something went wrong. Please try again later");
+            }
+        }
+    }
+}
