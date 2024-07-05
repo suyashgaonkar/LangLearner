bac03b471f496641fa57b62874dcb6799f0a98b4
Denyyyys <denys.fokashchuk@gmail.com>
2024-07-05 21:15:46 +0100

    Add basic authentication for users

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using LangLearner.Database.Repositories;
+using LangLearner.Exceptions;
+using LangLearner.Models.Dtos.Requests;
+using LangLearner.Models.Dtos.Responses;
+using LangLearner.Models.Entities;
+using LangLearner.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace LangLearner.Controllers
 {
@@ -7,5 +16,60 @@ namespace LangLearner.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+        public readonly IUserRepository _userRepository;
+        public readonly ILogger<UserController> _logger;
+        private readonly IMapper _mapper;
+
+
+        public UserController(IUserRepository userRepository, IUserService userService, IMapper mapper, ILogger<UserController> logger)
+        {
+            _userService = userService;
+            _userRepository = userRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<TokenDto> Register([FromBody] CreateUserDto userDto)
+        {
+            string? token = _userService.Register(userDto);
+            if (token == null || token == string.Empty)
+            {
+                // TODO
+                throw new Exception("Token cannot be empty!");
+            }
+            return CreatedAtAction(nameof(Register), new TokenDto() { Token=token});
+        }
+
+        [HttpPost("login")]
+        public ActionResult<TokenDto> Login([FromBody] LoginUserDto userDto)
+        {
+            string? token = _userService.Login(userDto);
+            if (token == null || token == string.Empty)
+                return Unauthorized(new ApiError() { ErrorMessage ="Bad credentials provided!", StatusCode=401});
+            return Ok(new TokenDto() { Token = token });
+        }
+
+        [Authorize]
+        [HttpGet("stats")]
+        public ActionResult<TokenDto> GetStats()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            int userId = int.Parse(User.Claims.First(c => c.Type == "UserId").Value);
+            User? user = _userRepository.GetUserById(userId);
+
+            if (user == null)
+                //throw new UnauthorizedException();
+                throw new Exception();
+
+            UserStatsDto userStatsDto = _mapper.Map<UserStatsDto>(user);
+
+            return Ok(userStatsDto);
+        }
+
     }
 }
