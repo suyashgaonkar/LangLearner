43f3d81f42b17541db5901276eace33c8a353c0f
Denyyyys <denys.fokashchuk@gmail.com>
2024-07-08 19:54:01 +0100

    Implement filter for checking ModelState of request for better API response

---
+++ backend/Exceptions/APIValidationException.cs
@@ -0,0 +1,16 @@
+﻿using LangLearner.Models.Dtos.Responses;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LangLearner.Exceptions
+{
+    public class APIValidationException : GeneralAPIException
+    {
+        public IDictionary<string, string[]> Errors { get; }
+
+        public APIValidationException(string message, IDictionary<string, string[]> Errors) : base(message)
+        {
+            this.StatusCode = 400;
+            this.Errors = Errors;
+        }
+    }
+}
---
+++ backend/Filters/ValidateModelFilter.cs
@@ -0,0 +1,28 @@
+﻿using LangLearner.Exceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.ComponentModel.DataAnnotations;
+
+namespace LangLearner.Filters
+{
+#pragma warning disable CS8602
+    public class ValidateModelFilter : IActionFilter
+    {
+
+        public void OnActionExecuting(ActionExecutingContext context) 
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState
+                .ToDictionary(
+                    kvp => kvp.Key,
+                    kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray()
+                );
+                throw new APIValidationException("Some fields are missing or invalid!", errors);
+            }
+        }
+        public void OnActionExecuted(ActionExecutedContext context) { }
+
+    }
+#pragma warning restore CS8602
+}
+
